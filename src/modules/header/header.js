e933f4a093335d1c83d3bfe46222ad926e14128d
vishal8288826277 <vishals@leewayhertz.com>
2022-07-29 11:29:17 +0530

    nft-ticketing

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className="">
       <div className=" bg-Slate-200 pt-28 h-screen ">
-        <div className="w-125  bg-white text-center  m-auto rounded-lg px-5  pb-12.5">
-          <img src="/images/img-logo.png" alt="" className="mx-auto pt-12.5 " />
+        <div className="w-125 h-135.5 bg-white text-center  m-auto rounded-lg px-5  pb-12.5">
+          <img src="/images/img-logo.png" alt="" className="mx-auto mt-15 " />
           <h1 className="text-ft7 font-OpenSansSemiBold pt-7.5">
             Login with nft ticketing
           </h1>
@@ -29,17 +29,21 @@ const Header = () => {
 
             <img alt="" src="" className={`pr-5  w-11.25 h-6.5 `} />
           </div>
-          {/* <div className="flex justify-between flex-row pt-7.5">
-          <div className=" flex">
-            <input type="checkbox" className="w-5 h-5 " value="" />
-            <p className="font-OpenSansRegular text-ft4 text-darkGrey-100 pl-2.5">
-              Remember me
-            </p>
+          <div className="flex justify-between flex-row pt-7.5">
+            <div className=" flex">
+              <input type="checkbox" className="w-5 h-5 " value="" />
+              <p className="font-OpenSansRegular text-ft4 text-darkGrey-100 pl-2.5">
+                Remember me
+              </p>
+            </div>
+            <p className="font-OpenSansRegular text-ft4 text-darkGrey-100 "></p>
           </div>
-          <p className="font-OpenSansRegular text-ft4 text-darkGrey-100 ">
-            <Link to="/forget-password">Forgot password?</Link>
-          </p>
-        </div> */}
+          <button
+            type="submit"
+            className="bg-blue-50 mt-12.5 py-6 text-ft4  text-white w-full rounded-full font-OpenSansSemiBold"
+          >
+            <div className="">Sign in</div>
+          </button>
         </div>
       </div>
     </div>
--- src/modules/main/mainComponent.js
+++ src/modules/main/mainComponent.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Billboard from '../billboard/index'
+import Billboard from "../billboard/index";
 import Header from "../header/header";
 
-function MainComponent(){
-    return (
-        <div className="global-container flex justify-center">
-            <div className="max-width-1640px">
-                <Header/>
-                <Billboard/>
-            </div>
-        </div>
-    )
+function MainComponent() {
+  return (
+    <div className="">
+      <div className="">
+        <Header />
+        {/* <Billboard /> */}
+      </div>
+    </div>
+  );
 }
 
 export default MainComponent;
